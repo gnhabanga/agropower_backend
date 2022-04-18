0845051bd2ef2dba527fd49c4644818f8a77199b
Gladistone Nhabanga <74876119+gnhabanga@users.noreply.github.com>
2022-04-18 14:57:08 +0200

    Update regiao.controller.js

--- src/controladores/regiao.controller.js
+++ src/controladores/regiao.controller.js
@@ -4,8 +4,10 @@ exports.getRegiaoModel = (req, res) => {
     RegiaoModel.getAllRegioes((err, regiao) => {
         console.log('localizacao actual');
         if (err)
+            res.header("Access-Control-Allow-Origin", "*");
             res.send(err);
         console.log('regioes', regiao);
+        res.header("Access-Control-Allow-Origin", "*");
         res.send(regiao);
 
     });
