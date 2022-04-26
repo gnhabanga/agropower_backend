f154f313524cd942b363591f79e5e2005b6d90f0
Gladistone <74876119+gnhabanga@users.noreply.github.com>
2022-04-26 18:41:47 +0200

    updated

--- src/controladores/regiao.controller.js
+++ src/controladores/regiao.controller.js
@@ -1,13 +1,14 @@
 const RegiaoModel = require('../models/regiao.model');
 
 exports.getRegiaoModel = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     RegiaoModel.getAllRegioes((err, regiao) => {
         console.log('localizacao actual');
         if (err)
-            res.header("Access-Control-Allow-Origin", "*");
+            
             res.send(err);
         console.log('regioes', regiao);
-        res.header("Access-Control-Allow-Origin", "*");
+        
         res.send(regiao);
 
     });
